0956fab83ba4c287c071dea97934b00614bae07e
Harshith Thota <harshith.thota7@gmail.com>
2017-08-02 22:05:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,15 +17,15 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/article-one', function (req, res) {
+app.get('/ui/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('/article-two', function (req, res) {
+app.get('/ui/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
-app.get('/article-three', function (req, res) {
+app.get('/ui/article-three', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
