2e91511498545c08735145b2fe143b71b3256ebc
Harshith Thota <harshith.thota7@gmail.com>
2017-08-02 23:29:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,15 +18,15 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+  res.send(createTemplate(articleTwo));
 });
 
 app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+  res.send(createTemplate(articleThree));
 });
 
 // Do not change port, otherwise your app won't run on IMAD servers
@@ -38,10 +38,59 @@ app.listen(port, function () {
 });
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-
-
-
+var articleOne = {
+    title: "You are viewing Article One!",
+    date: "2nd August",
+    heading: "This is article one.",
+    content: `<div>
+                <p>This is just a dummy article.</p>
+            </div>`
+};
+var articleTwo = {
+    title: "You are viewing Article Two!",
+    date: "2nd August",
+    heading: "This is article two.",
+    content: `<div>
+                <p>This is just a dummy article.</p>
+            </div>`
+};
+var articleThree = {
+    title: "You are viewing Article Three!",
+    date: "2nd August",
+    heading: "This is article three.",
+    content: `<div>
+                <p>This is just a dummy article.</p>
+            </div>`
+};
+function createTemplate(data){
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    var htmlTemplate =
+        `<html>
+            <head>
+                <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <meta name = "author" content = "Harshith Thota"/>
+                <meta charset="utf-8">
+                <title>
+                    $(title)
+                </title>    
+            </head>
+            <body>
+                <div class="container">
+                    <div>
+                        <h1>$(heading)</h1>
+                        <p>$(date)</p>
+                    </div>
+                    $(content)
+                </div>
+                <footer>Click <a href = "/">here</a> to go back to the homepage.</footer>
+            </body>            
+         </html>`;
+         return htmlTemplate;
+}
 
 
 
