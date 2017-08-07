902fb54af909b3cb1e3af7982e4d078ca4d1d607
Harshith Thota <harshith.thota7@gmail.com>
2017-08-07 16:53:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,12 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+var counter = 0;
+app.get('/counter', function (){
+  counter = counter + 1;
+  res.send(counter.toString());
+});
+
 app.get('/:articleName', function (req, res) {
   var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
