e5fdc51d42bf979377c6caed8bf2f5fc33c652e9
Harshith Thota <harshith.thota7@gmail.com>
2017-08-07 20:08:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,13 @@
-console.log('Loaded!');
+console.log('Counter Incremented!');
+var request = new XMLHttpRequest();
+request.onreadystatechange = function(){
+    if (request.readyState === XMLHttpRequest.DONE){
+        if (request.status === 200){
+            var cnt = request.responseText;
+            var chng = document.getElementById("countText");
+            chng.value = cnt.toString();
+        }
+    }
+};
+request.open('GET','http://dragonlordthota717.imad.hasura-app.io/counter', true);
+request.send(null);
