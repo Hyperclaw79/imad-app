3747223c9ce91ca4428997a28a6b69ea4de1bd15
Harshith Thota <harshith.thota7@gmail.com>
2017-08-10 00:27:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ function updateComment(comment){
         }
         colList[k]= col;
     }
-    var subTemplate = `<div class="bubble-list" style="padding-left:15%>
+    var subTemplate = `<div class="bubble-list">
              <div class="bubble clearfix">
                 <img src="https://avatars3.githubusercontent.com/u/29298411?v=4&s=400"/ style = "border: 3px solid ${colList[i]};width:80px;height:80px;"/>
              <div class="bubble-content">
