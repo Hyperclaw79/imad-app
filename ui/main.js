fdaab33f0a483b646567f81acb9b7db1edacf622
Harshith Thota <harshith.thota7@gmail.com>
2017-08-09 20:20:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,6 +17,7 @@ button.onclick = function(){
     var commText = document.getElementById('commText').value;
     var reqC = new XMLHttpRequest();
     url = 'http://dragonlordthota717.imad.hasura-app.io/submit-comment?comm='+commText;
+    window.open(url);
     request.open('GET',url, true);
     request.send(null);
 };
