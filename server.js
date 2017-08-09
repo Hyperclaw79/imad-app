bfd1015ef461db60b1c41a461101092cee4d9bca
Harshith Thota <harshith.thota7@gmail.com>
2017-08-10 00:54:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -198,8 +198,8 @@ function updateComment(comment){
     	</style>
        </head>
        <body>
-          <div style="margin: 30px;background-color: rgba(255, 255, 255, 0.4);border: 3px solid black;box-shadow:10px 10px 5px rgba(0,0,0,.2);"><h1 style="text-shadow: 3px 3px 5px blue;">Here is the anonymous comment feed:</h1></div>
-          <div style="margin: 30px;background-color: rgba(255, 255, 255, 0.4);border: 5px solid black;box-shadow:10px 10px 5px rgba(0,0,0,.2);">
+          <div style="margin: 30px;background-color: rgba(255, 255, 255, 0.4);border: 3px solid black;box-shadow:10px 10px 5px rgba(0,0,0,.2);border-radius:10px;"><h1 style="text-shadow: 3px 3px 5px blue;">Here is the anonymous comment feed:</h1></div>
+          <div style="margin: 30px;background-color: rgba(255, 255, 255, 0.4);border: 5px solid black;box-shadow:10px 10px 5px rgba(0,0,0,.2);border-radius:10px;">
           ${subTemplate}
           </div>
        </body>
