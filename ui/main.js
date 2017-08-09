99522e49987573979855f72d8ae09bb66d5bfac6
Harshith Thota <harshith.thota7@gmail.com>
2017-08-09 20:57:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,3 @@
-var button = document.getElementById('commSub');
-button.onclick = function(){
-    var commText = document.getElementById('commText').value;
-    var reqC = new XMLHttpRequest();
-    url = 'http://dragonlordthota717.imad.hasura-app.io/submit-comment?comm='+commText;
-    window.open(url);
-    request.open('GET',url, true);
-    request.send(null);
-};
 function loader(){
     console.log('Counter Incremented!');
     var request = new XMLHttpRequest();
