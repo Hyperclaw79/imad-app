47c3fdedd71866ea990e7d29380e862069f82161
Harshith Thota <harshith.thota7@gmail.com>
2017-08-11 19:29:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,24 +12,4 @@ function loader(){
     };
     request.open('GET','http://dragonlordthota717.imad.hasura-app.io/counter', true);
     request.send(null);
-}    
-
-function exec(){
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() 
-    {
-          if (request.readyState === XMLHttpRequest.DONE) 
-          {
-              //Take Some Action
-              if (request.status === 200) 
-                { 
-                    var counter = request.responseText; 
-                    var span = document.getElementById('count');
-                    span.innerHTML = counter.toString();
-                } 
-          } 
-    }; 
-    request.open("GET", "http://dragonlordthota717.imad.hasura-app.io/counter", true);
-    request.send(); 
-    console.log('EXECUTED');
 }
