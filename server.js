04233840b0df02a280ef5860e244f8b22d7019e5
Harshith Thota <harshith.thota7@gmail.com>
2017-08-14 12:12:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get('/counter', function (req, res){
         res.status(500).send(err.toString());
     }
     else{
-        counter = JSON.parse(result);
+        counter = JSON.stringify(result.rows[0]).pageviews;
     }
   });
   //counter = counter + 1;
