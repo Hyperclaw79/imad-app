aa20afdd85292e132e2bcee1bf9e366b6eeacf03
Harshith Thota <harshith.thota7@gmail.com>
2017-08-14 10:24:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
 var app = express();
 app.use(morgan('combined'));
 
+var Pool = require('pg').Pool;
+var config = {
+    user: 'dragonlordthota717',
+    database: 'dragonlordthota717',
+    host: 'http://db.imad.hasura-app.io',
+    port: '5432',
+    password: proces.env.DB_PASSWORD
+};
+var pool = new Pool(config);
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/userlist', function(req,res){
+    
+    pool.query('SELECT name FROM users',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send(JSON.stringify(result));
+        }
+    });
 });
 
 app.get('/ui/style.css', function (req, res) {
