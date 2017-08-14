db79fb18b5b2dfdb1b29300ce771dfa40ddefb19
Harshith Thota <harshith.thota7@gmail.com>
2017-08-14 11:22:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,9 +57,23 @@ app.get('/submit-comment', function (req, res) {
   res.send(updateComment(comment));
 });
 
-app.get('/:articleName', function (req, res) {
+app.get('/article/:articleName', function (req, res) {
   var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+  pool.query("SELECT * FROM article WHERE title="+articleName,function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            if(result.rows.length===0){
+                res.status(404).send('Article Not Found.');
+            }
+            else{
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+                
+        }
+    });
 });
 
 // Do not change port, otherwise your app won't run on IMAD servers
