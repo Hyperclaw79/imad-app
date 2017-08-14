7880b39f9e12cbd4e85946851c8797b0dde047ee
Harshith Thota <harshith.thota7@gmail.com>
2017-08-14 12:33:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.get('/counter', function (req, res){
       return cnt; //.toString();
   }
   //counter = counter + 1;
-  res.send(Object.keys(counter));
+  res.send(Object.values(counter));
   /*pool.query(`UPDATE "users" SET "pageviews" = $1 WHERE "name" = 'Hyperclaw79';`,[counter],function(err,result){
     if(err){
         res.status(500).send(err.toString());
