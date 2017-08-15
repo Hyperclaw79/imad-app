cd7b544f1f8bdbc685ff04bb1e3d7b5f8dbcc67a
Harshith Thota <harshith.thota7@gmail.com>
2017-08-15 12:28:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ function hash(input,salt){
 }
 
 app.get('/hash/:input',function(req,res){
-    var hashedString = hash(req.params.input,'this-is-some-random-string');
+    var hashedString = hash(req.params.input,'salt_this_up_Jarvis');
     res.send(hashedString);
 });
 
