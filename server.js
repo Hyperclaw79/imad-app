46822f204b2839e55d58b77b2bd251864d62c7ea
Harshith Thota <harshith.thota7@gmail.com>
2017-08-15 13:18:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.get('/submit-comment', function (req, res) {
                     res.status(500).send(err.toString());
                 }
                 else{
-                    res.send(result);
+                    res.send(result.rows[0]);
                 }
             });
         }
