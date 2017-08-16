542b07bc4379a7d182a5b633c575faf28d947d3b
Harshith Thota <harshith.thota7@gmail.com>
2017-08-16 09:48:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -174,7 +174,7 @@ app.get('/check-session',function(req,res){
         return res.send('Active');
     }
     else{
-        return res.send('Inactive: '+req.session.auth);
+        return res.send('Inactive: '+req.session);
     }
 });
 
