dc190b0b8eb840f3b9a6f4c6ba82193c40ed2d75
Harshith Thota <harshith.thota7@gmail.com>
2017-08-17 22:04:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -58,6 +58,7 @@ function verify(){
             else if (request.responseText === 'Account does not exist. Please Register.'){
                 if(confirm("That username doesn't exist. Click OK to register now.")=== true){
                     $('.registration-form').show();
+                    register();
                 }
             }
             else{
@@ -72,7 +73,148 @@ function verify(){
     request.send(JSON.stringify({"username":uname,"password":pwd}));
 }
 
+function checkUser(data){
+    var request = new XMLHttpRequest();
+    var uname = data;
+    request.open('POST','http://dragonlordthota717.imad.hasura-app.io/check-user', true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({"username":uname}));
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE){
+            if (request.status === 403){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+    };
+}
 
+function register(){
+   $('.register_username').on("change keyup paste",
+      function(){
+        if($(this).val()){
+            if(checkUser($(this).val())){ 
+                var uname = $(this).val();
+                $('.icon-uname').addClass("next");
+            }
+            else alert('Account already exists. Please Login.');
+        } 
+        else {
+          $('.icon-uname').removeClass("next");
+        }
+      }
+    );
+    
+    $('.next-button').hover(
+      function(){
+        $(this).css('cursor', 'pointer');
+      }
+    );
+    
+    $('.next-button.username').click(
+      function(){
+        $('.username-section').addClass("fold-up");
+        $('.name-section').removeClass("folded");
+      }
+    );
+    $('.register_name').on("change keyup paste",
+      function(){
+        if($(this).val()){
+          var nname = $(this).val();        
+          $('.icon-name').addClass("next");
+        } else {
+          $('.icon-name').removeClass("next");
+        }
+      }
+    );
+    
+    $('.next-button').hover(
+      function(){
+        $(this).css('cursor', 'pointer');
+      }
+    );
+    
+    $('.next-button.name').click(
+      function(){
+        $('.name-section').addClass("fold-up");
+        $('.email-section').removeClass("folded");
+      }
+    );
+    
+    $('.email').on("change keyup paste",
+      function(){
+         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(re.test($(this).val())){
+          var profile = $(this).val();    
+          $('.icon-paper-plane').addClass("next");
+        } else {
+          $('.icon-paper-plane').removeClass("next");
+        }
+      }
+    );
+    
+    $('.next-button').hover(
+      function(){
+        $(this).css('cursor', 'pointer');
+      }
+    );
+    
+    $('.next-button.email').click(
+      function(){
+        $('.email-section').addClass("fold-up");
+        $('.password-section').removeClass("folded");
+      }
+    );
+     
+    $('.password').on("change keyup paste",
+      function(){
+        if($(this).val()){
+          $('.icon-lock').addClass("next");
+        } else {
+          $('.icon-lock').removeClass("next");
+        }
+      }
+    );
+    
+    $('.next-button').hover(
+      function(){
+        $(this).css('cursor', 'pointer');
+      }
+    );
+    
+    $('.next-button.password').click(
+      function(){
+        $('.password-section').addClass("fold-up");
+        $('.repeat-password-section').removeClass("folded");
+      }
+    );
+    
+    $('.repeat-password').on("change keyup paste",
+      function(){
+        if($(this).val()===$('.password').val()){
+          $('.icon-repeat-lock').addClass("next");
+          var pwd = $(this).val();
+        } else {
+          $('.icon-repeat-lock').removeClass("next");
+        }
+      }
+    );
+    
+    $('.next-button.repeat-password').click(
+      function(){
+        $('.repeat-password-section').addClass("fold-up");
+        $('.success').css("marginTop", 0);
+      }
+    );
+
+    var request = new XMLHttpRequest();
+    request.open('POST','http://dragonlordthota717.imad.hasura-app.io/register', true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({"username":uname,"name":nname,"imad-profile":profile,"password":pwd}));
+
+}
 
 function logout(){
     var request = new XMLHttpRequest();
