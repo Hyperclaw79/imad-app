cad1574c3589e99a7d199757872c3a855d1dc474
Harshith Thota <harshith.thota7@gmail.com>
2017-08-20 16:31:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -95,151 +95,159 @@ function checkUser(data){
     request.send(JSON.stringify({"username":uname}));
 }
 
-function register(){
-   var uname = "";
-   var nname = "";
-   var profile = "";
-   var pwd = "";    
-   $('.register_username').on("change keyup paste",
-      function(){
-        if($(this).val()){
-            $('.icon-uname').addClass("next");            
-        } 
-        else {
-            $('.icon-uname').removeClass("next");
+function register() {
+    var uname = "";
+    var nname = "";
+    var profile = "";
+    var pwd = "";
+    $('.register_username').on("change keyup paste",
+        function() {
+            if ($(this).val()) {
+                $('.icon-uname').addClass("next");
+            } else {
+                $('.icon-uname').removeClass("next");
+            }
         }
-      }
     );
-    
+
     $('.next-button').hover(
-      function(){
-        $(this).css('cursor', 'pointer');
-      }
-    );
-    
-    $('.next-button.username').click(function(){
-            var request = new XMLHttpRequest();
-            var uname = $('.register_username').val();
-            var toggle = "stateless";
-            request.onreadystatechange = function(){
-                if (request.readyState === XMLHttpRequest.DONE){
-                    if (request.status === 403){
-                        alert('Account already exists. Please Login.');
-                    }
-                    else if(request.status === 200){
-                        $('.username-section').addClass("fold-up");
-                        $('.name-section').removeClass("folded");
-                    }
-                }
-            };
-            request.open('POST','http://dragonlordthota717.imad.hasura-app.io/check-user', true);
-            request.setRequestHeader('Content-Type','application/json');
-            request.send(JSON.stringify({"username":uname}));
-                
-    });
-    $('.register_name').on("change keyup paste",
-      function(){
-        if($(this).val()){
-          $('.icon-name').addClass("next");
-        } else {
-          $('.icon-name').removeClass("next");
+        function() {
+            $(this).css('cursor', 'pointer');
         }
-      }
     );
-    
-    $('.next-button').hover(
-      function(){
-        $(this).css('cursor', 'pointer');
-      }
-    );
-    
-    $('.next-button.name').click(
-      function(){
-        nname = $('register_name').val();        
-        $('.name-section').addClass("fold-up");
-        $('.email-section').removeClass("folded");
-      }
-    );
-    
-    $('.email').on("change keyup paste",
-      function(){
-         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if(re.test($(this).val())){
-          profile = $(this).val();    
-          $('.icon-paper-plane').addClass("next");
-        } else {
-          $('.icon-paper-plane').removeClass("next");
-        }
-      }
-    );
-    
-    $('.next-button').hover(
-      function(){
-        $(this).css('cursor', 'pointer');
-      }
-    );
-    
-    $('.next-button.email').click(
-      function(){
-        $('.email-section').addClass("fold-up");
-        $('.password-section').removeClass("folded");
-      }
-    );
-     
-    $('.password').on("change keyup paste",
-      function(){
-        if($(this).val()){
-          $('.icon-lock').addClass("next");
-        } else {
-          $('.icon-lock').removeClass("next");
-        }
-      }
-    );
-    
-    $('.next-button').hover(
-      function(){
-        $(this).css('cursor', 'pointer');
-      }
-    );
-    
-    $('.next-button.password').click(
-      function(){
-        $('.password-section').addClass("fold-up");
-        $('.repeat-password-section').removeClass("folded");
-      }
-    );
-    
-    $('.repeat-password').on("change keyup paste",
-      function(){
-        if($(this).val()===$('.password').val()){
-          $('.icon-repeat-lock').addClass("next");
-          pwd = $(this).val();
-        } else {
-          $('.icon-repeat-lock').removeClass("next");
-        }
-      }
-    );
-    
-    $('.next-button.repeat-password').click(
-      function(){
-        uname = $('.register_username').val();
-        var reg = new XMLHttpRequest();
-        reg.onreadystatechange = function(){
-            if (reg.readyState === XMLHttpRequest.DONE){
-                if (reg.status === 200){
-                    $('.repeat-password-section').addClass("fold-up");
-                    $('.success').css("marginTop", 0);
-                }
-                else{
-                    alert(reg.responseText);    
+
+    $('.next-button.username').click(function() {
+        var request = new XMLHttpRequest();
+        var uname = $('.register_username').val();
+        var toggle = "stateless";
+        request.onreadystatechange = function() {
+            if (request.readyState === XMLHttpRequest.DONE) {
+                if (request.status === 403) {
+                    alert('Account already exists. Please Login.');
+                } else if (request.status === 200) {
+                    $('.username-section').addClass("fold-up");
+                    $('.name-section').removeClass("folded");
+                    $('.register_name').on("change keyup paste",
+                        function() {
+                            if ($(this).val()) {
+                                $('.icon-name').addClass("next");
+                            } else {
+                                $('.icon-name').removeClass("next");
+                            }
+                        }
+                    );
+
+                    $('.next-button').hover(
+                        function() {
+                            $(this).css('cursor', 'pointer');
+                        }
+                    );
+
+                    $('.next-button.name').click(
+                        function() {
+                            nname = $('register_name').val();
+                            $('.name-section').addClass("fold-up");
+                            $('.email-section').removeClass("folded");
+                            $('.email').on("change keyup paste",
+                                function() {
+                                    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                                    if (re.test($(this).val())) {
+                                        profile = $(this).val();
+                                        $('.icon-paper-plane').addClass("next");
+                                    } else {
+                                        $('.icon-paper-plane').removeClass("next");
+                                    }
+                                }
+                            );
+
+                            $('.next-button').hover(
+                                function() {
+                                    $(this).css('cursor', 'pointer');
+                                }
+                            );
+
+                            $('.next-button.email').click(
+                                function() {
+                                    $('.email-section').addClass("fold-up");
+                                    $('.password-section').removeClass("folded");
+                                    $('.password').on("change keyup paste",
+                                        function() {
+                                            if ($(this).val()) {
+                                                $('.icon-lock').addClass("next");
+                                            } else {
+                                                $('.icon-lock').removeClass("next");
+                                            }
+                                        }
+                                    );
+
+                                    $('.next-button').hover(
+                                        function() {
+                                            $(this).css('cursor', 'pointer');
+                                        }
+                                    );
+
+                                    $('.next-button.password').click(
+                                        function() {
+                                            $('.password-section').addClass("fold-up");
+                                            $('.repeat-password-section').removeClass("folded");
+                                            $('.repeat-password').on("change keyup paste",
+                                                function() {
+                                                    if ($(this).val() === $('.password').val()) {
+                                                        $('.icon-repeat-lock').addClass("next");
+                                                        pwd = $(this).val();
+                                                    } else {
+                                                        $('.icon-repeat-lock').removeClass("next");
+                                                    }
+                                                }
+                                            );
+
+                                            $('.next-button.repeat-password').click(
+                                                function() {
+                                                    uname = $('.register_username').val();
+                                                    var reg = new XMLHttpRequest();
+                                                    reg.onreadystatechange = function() {
+                                                        if (reg.readyState === XMLHttpRequest.DONE) {
+                                                            if (reg.status === 200) {
+                                                                $('.repeat-password-section').addClass("fold-up");
+                                                                $('.success').css("marginTop", 0);
+                                                            } else {
+                                                                alert(reg.responseText);
+                                                            }
+                                                        }
+                                                    };
+                                                    reg.open('POST', 'http://dragonlordthota717.imad.hasura-app.io/register', true);
+                                                    reg.setRequestHeader('Content-Type', 'application/json');
+                                                    reg.send(JSON.stringify({
+                                                        "username": uname,
+                                                        "name": nname,
+                                                        "imad-profile": profile,
+                                                        "password": pwd
+                                                    }));
+
+                                                });
+                                        }
+                                    );
+
+
+                                }
+                            );
+
+
+                        }
+                    );
+
+
                 }
             }
         };
-        reg.open('POST','http://dragonlordthota717.imad.hasura-app.io/register', true);
-        reg.setRequestHeader('Content-Type','application/json');
-        reg.send(JSON.stringify({"username":uname,"name":nname,"imad-profile":profile,"password":pwd}));
-        
+        request.open('POST', 'http://dragonlordthota717.imad.hasura-app.io/check-user', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({
+            "username": uname
+        }));
+
     });
+
 }
 
 function logout(){
