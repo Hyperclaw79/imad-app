cf1f6134ddf9ccfea944c1c64f87e74cd0ddfbbe
Harshith Thota <harshith.thota7@gmail.com>
2017-08-22 20:37:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -213,7 +213,7 @@ function register() {
                                                                 setTimeout(function(){
                                                                     $('.registration-form').siblings().css({opacity:1});
                                                                 },25000);
-                                                                    $('.registration-form').css({height:0,transition: height 2s linear});
+                                                                    $('.registration-form').animate({height:0,opacity:0},2000);
                                                             } else {
                                                                 alert(reg.responseText);
                                                             }
